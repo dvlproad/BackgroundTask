12a31b1f87f631f679436407debab1eaa730ae86
dvlproad <913168921@qq.com>
2015-11-10 18:18:41 +0800

    update

---
+++ BackgroundTaskDemo/BackgroundTaskDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BackgroundTaskDemo
+//
+//  Created by lichq on 15/10/15.
+//  Copyright (c) 2015年 李超前. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BackgroundTaskDemo/BackgroundTaskDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BackgroundTaskDemo
+//
+//  Created by lichq on 15/10/15.
+//  Copyright (c) 2015年 李超前. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
